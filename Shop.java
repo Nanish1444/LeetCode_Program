4073790e31d9244217423ed3e4c54651a55d7194
Nanish1444 <125627633+Nanish1444@users.noreply.github.com>
2024-10-08 01:39:04 +0530

    Update Shop.java

--- Shop.java
+++ Shop.java
@@ -1,72 +1,37 @@
 import java.util.*;
 class Basket{
-	private  int index;
-	private String[] str;
-	Basket nextBasket;
+	private count;
+	private Fruit[] fruits;
 	Basket(){
-		nextBasket=null;
-		index=0;
-		str=new String[10];
-		Arrays.fill(str,"Empty");
+		count=0;
+		fruits=new Fruit[10];
 	}
-
-	public boolean Add(String item){
-		if(index<10){
-			str[index++]=item;
-			return true;
+	void addToBasket(String name){
+		if(count<10){
+			fruits[count]=new Fruit(name);
+			count++;
 		}
-		return false;
+	}
+	void removeFromBasket(){
+		if(count>=0){
+			fruits[count]="empty";
+			count--;
+		}
+
 	}
 	
-	@Override
-	public String toString(){
-		String value="[";
-		for(int i=0;i<str.length;i++){
-			value+=str[i];
-			if(i<str.length-1){
-				value+=",";
-			}
-		}
-		value+="]";
-		return value;
-	}
 }
-
-class Fruits{
-	Basket basket;
-	Fruits(){
-		basket=new Basket();
-	}
-	void buy(String item){
-		Basket temp=basket;
-		while(temp.nextBasket!=null){
-			temp=temp.nextBasket;
-		}
-		if(temp.nextBasket==null&&temp.Add(item)==false){
-			temp.nextBasket=new Basket();
-			temp=temp.nextBasket;
-			temp.Add(item);
-		}
-
-
-	}
-	void viewBasket(){
-		Basket temp=basket;
-		while(temp!=null){
-			System.out.println(temp);
-			temp=temp.nextBasket;
-		}
-		
+class Fruit{
+	private String name;
+	Fruit(String name){
+		this.name=name;
 	}
 
 }
 class Shop{
 	public static void main(String[]args){
-		String[] things={"1 Apple ","2 Banana ","3 Blueberry ","4 Cherry ","5 Grapefruit ","6 Kiwi ","7 Lemon ","8 Mango ","9 Orange ","10 Strawberry ","11","12","13","14","15"};
-		Fruits fruits=new Fruits();
-		for(int i=0;i<things.length;i++){
-			fruits.buy(things[i]);
-		}
-		fruits.viewBasket();
+		Basket basket=new Basket();
+		basket.addToBasket("Apple");
+		
 	}
 }
