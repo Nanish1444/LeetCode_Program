cd9321c4e1d60c30baf90729c9ad0206b3c55144
Nanish1444 <125627633+Nanish1444@users.noreply.github.com>
2024-10-25 14:25:59 +0530

    Update 18. 4Sum.java

--- 18. 4Sum.java
+++ 18. 4Sum.java
@@ -4,6 +4,9 @@ class Solution {
         List<List<Integer>> list = new ArrayList<>();
         if(nums.length<4)return list;
         for(int i=0;i<nums.length-3;i++){
+            if(i>=0 && nums[i]==nums[i-1]){
+                continue;
+            }
             for(int j=i+1;j<nums.length;j++){
                 int k=j+1;
                 int l=nums.length-1;
