54c05e5540fe6807ad096017aaad324605617b58
Nanish1444 <125627633+Nanish1444@users.noreply.github.com>
2024-10-25 14:25:10 +0530

    Create 18. 4Sum.java

---
+++ 18. 4Sum.java
@@ -0,0 +1,31 @@
+class Solution {
+    public List<List<Integer>> fourSum(int[] nums, int target) {
+        Arrays.sort(nums);
+        List<List<Integer>> list = new ArrayList<>();
+        if(nums.length<4)return list;
+        for(int i=0;i<nums.length-3;i++){
+            for(int j=i+1;j<nums.length;j++){
+                int k=j+1;
+                int l=nums.length-1;
+                while(k<l){
+                    long sum=(long)nums[i]+nums[j]+nums[k]+nums[l];
+                    if(sum==target){
+                        List<Integer> temp=List.of(nums[i],nums[j],nums[k],nums[l]);
+                        if(!list.contains(temp)){
+                            list.add(List.of(nums[i],nums[j],nums[k],nums[l]));
+                        }
+                        
+                        k++;
+                    }
+                    else if(sum>target){
+                        l--;
+                    }
+                    else{
+                        k++;
+                    }
+                }
+            }
+        }
+        return list;
+    }
+}
